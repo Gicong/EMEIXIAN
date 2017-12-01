9c2ddeb1119d0f19df9da1aba60bc3f14a362659
2862171636 <31610975+2862171636@users.noreply.github.com>
2017-12-01 21:39:53 +0800

    周阳
    创建了一个User类

--- EMeiXian/src/main/java/com/lanou/dao/UserMapper.java
+++ EMeiXian/src/main/java/com/lanou/dao/UserMapper.java
@@ -4,4 +4,7 @@ package com.lanou.dao;
  * Created by lanou on 2017/12/1.
  */
 public class UserMapper {
+
+
+
 }
---
+++ EMeiXian/src/main/java/com/lanou/entity/User.java
@@ -0,0 +1,8 @@
+package com.lanou.entity;
+
+/**
+ * Created by lanou on 2017/12/1.
+ */
+public class User {
+    private int uId;
+}
